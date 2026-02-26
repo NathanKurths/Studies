8d9d556433f8a1d7496b1e58edd352ab8c2fdc3c
Nathan Kurths <nathankurthsm@gmail.com>
2026-02-26 19:49:24 -0300

    commit

--- review_python/functions.py
+++ review_python/functions.py
@@ -1,9 +1,13 @@
-def make_album(nome_artista, album):
-    disco = {'Nome': nome_artista, 'Album': album}
-    return disco
+def show_magicians(lista):
+    print(lista)
 
-while True:
-    nome_artista = input('\nDigite o nome do artista:')
-    album = input('Digite o nome do álbum:')
-    musico = make_album(nome_artista, album)
-    print((musico))
+def make_great(lista, aux):
+    while lista:
+        var = lista.pop()
+        aux.append('O Grande ' + var)
+
+lista = ['joao','nathan','pedro']
+lista_nova = []
+make_great(lista[:], lista_nova)
+print(lista)
+print(lista_nova)
